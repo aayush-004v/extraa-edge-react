c2c292f9471c853e75d70b14dcb05a20860039e1
AAYUSH VERMA <106806750+aayush-004v@users.noreply.github.com>
2022-12-02 11:41:24 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
+import { useEffect, useState } from 'react';
 import './App.css';
 import FrontPage from './Components/FrontPage';
+import Loader from './Components/Loader';
 function App() {
+
+  const [flag, setFlag] = useState(false);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setFlag(true);
+    }, 2500);
+  }, [flag])
+
   return (
     <>
-     <FrontPage/>
+      {
+        flag ? <FrontPage /> : <Loader />
+      }
+
     </>
   );
 }
