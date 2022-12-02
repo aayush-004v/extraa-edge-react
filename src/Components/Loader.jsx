38873e7e91336f9fb8adf4934afae4e9a8260ec6
AAYUSH VERMA <106806750+aayush-004v@users.noreply.github.com>
2022-12-02 11:41:01 +0530

    Create Loader.jsx

---
+++ src/Components/Loader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Loader = () => {
+  return (
+    <div className='loader'
+      
+    >
+      <img src="extraa-edge-loader.gif" alt="extraa-edge-loader" />
+    </div>
+  );
+}
+
+export default Loader
